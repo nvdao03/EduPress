237f1a433c3ee8aa0791c5098f24c8e90c021b5a
Nguyen Van Dao <nvdao003@gmail.com>
2025-03-03 20:30:08 +0700

    UI page login and register

--- client/public/assets/js/app.js
+++ client/public/assets/js/app.js
@@ -1,10 +1,10 @@
 /*
   1. Load data header, footer
 */
-document.addEventListener('DOMContentLoaded', function () {
-    var header = document.getElementById('header');
-    var footer = document.getElementById('footer');
-    fetch('/client/public/template/header.html')
+document.addEventListener("DOMContentLoaded", function () {
+    var header = document.getElementById("header");
+    var footer = document.getElementById("footer");
+    fetch("/client/public/template/header.html")
         .then(function (response) { return response.text(); })
         .then(function (data) {
         header.innerHTML = data;
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
         .catch(function (error) {
         console.log(error.message);
     });
-    fetch('/client/public/template/footer.html')
+    fetch("/client/public/template/footer.html")
         .then(function (response) { return response.text(); })
         .then(function (data) {
         footer.innerHTML = data;
@@ -21,3 +21,59 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log(error.message);
     });
 });
+/*
+  2. Page Register
+*/
+var handleButtonRegister = function () {
+    var btnRegister = document.getElementById("btn-register");
+    var email = document.getElementById("email");
+    var userName = document.getElementById("username");
+    var password = document.getElementById("password");
+    var confirmPassword = document.getElementById("confirmPassword");
+    // Sự kiện "input" theo dõi giá trị thay đổi của ô input. Cho các item vào mảng và dùng for để lặp qa từng giá trị để kiểm tra
+    [email, userName, password, confirmPassword].forEach(function (input) {
+        input.addEventListener("input", function () {
+            if (email.value.trim() !== "" &&
+                userName.value.trim() !== "" &&
+                password.value.trim() !== "" &&
+                confirmPassword.value.trim() !== "" &&
+                password.value === confirmPassword.value) {
+                btnRegister.classList.remove("opacity-50");
+                btnRegister.disabled = false;
+            }
+            else {
+                btnRegister.classList.add("opacity-50");
+            }
+        });
+    });
+    btnRegister.onclick = function () {
+        window.location.href = "./login.html";
+    };
+};
+var handleShowPassword = function () {
+    var password = document.getElementById("password");
+    var confirmPassword = document.getElementById("confirmPassword");
+    var eyeIconPassWord = document.getElementById("eye-password");
+    var eyeIconConfirm = document.getElementById("eye-confirm");
+    eyeIconPassWord.addEventListener("click", function () {
+        if (password.type === "password") {
+            password.type = "text";
+        }
+        else {
+            password.type = "password";
+        }
+    });
+    eyeIconConfirm.addEventListener("click", function () {
+        if (confirmPassword.type === "password") {
+            confirmPassword.type = "text";
+        }
+        else {
+            confirmPassword.type = "password";
+        }
+    });
+};
+var app = function () {
+    handleButtonRegister();
+    handleShowPassword();
+};
+app();
--- client/src/assets/ts/app.ts
+++ client/src/assets/ts/app.ts
@@ -1,27 +1,89 @@
 /*
   1. Load data header, footer
 */
-document.addEventListener('DOMContentLoaded', () => {
-  const header: HTMLElement | any = document.getElementById('header');
-  const footer: HTMLElement | any = document.getElementById('footer');
+document.addEventListener("DOMContentLoaded", () => {
+  const header: HTMLElement | any = document.getElementById("header");
+  const footer: HTMLElement | any = document.getElementById("footer");
 
-  fetch('/client/public/template/header.html')
+  fetch("/client/public/template/header.html")
     .then((response) => response.text())
     .then((data) => {
       header.innerHTML = data;
     })
     .catch((error) => {
       console.log(error.message);
-      
-    })
+    });
 
-  fetch('/client/public/template/footer.html')
+  fetch("/client/public/template/footer.html")
     .then((response) => response.text())
     .then((data) => {
       footer.innerHTML = data;
     })
     .catch((error) => {
       console.log(error.message);
-      
-  })
-})
+    });
+});
+
+/*
+  2. Page Register
+*/
+const handleButtonRegister = () => {
+  const btnRegister = document.getElementById("btn-register") as HTMLButtonElement;
+  const email = document.getElementById("email") as HTMLInputElement;
+  const userName = document.getElementById("username") as HTMLInputElement;
+  const password = document.getElementById("password") as HTMLInputElement;
+  const confirmPassword = document.getElementById("confirmPassword") as HTMLInputElement;
+
+  // Sự kiện "input" theo dõi giá trị thay đổi của ô input. Cho các item vào mảng và dùng for để lặp qa từng giá trị để kiểm tra
+  [email, userName, password, confirmPassword].forEach((input) => {
+    input.addEventListener("input", () => {
+      if (
+        email.value.trim() !== "" &&
+        userName.value.trim() !== "" &&
+        password.value.trim() !== "" &&
+        confirmPassword.value.trim() !== "" &&
+        password.value === confirmPassword.value
+      ) {
+        btnRegister.classList.remove("opacity-50");
+        btnRegister.disabled = false;
+      } else {
+        btnRegister.classList.add("opacity-50");
+      }
+    });
+  });
+
+  btnRegister.onclick = () => {
+    window.location.href = "./login.html";
+  };
+};
+
+const handleShowPassword = () => {
+  const password = document.getElementById("password") as HTMLInputElement;
+  const confirmPassword = document.getElementById("confirmPassword") as HTMLInputElement;
+
+  const eyeIconPassWord = document.getElementById("eye-password") as HTMLDivElement;
+  const eyeIconConfirm = document.getElementById("eye-confirm") as HTMLDivElement;
+
+  eyeIconPassWord.addEventListener("click", () => {
+    if (password.type === "password") {
+      password.type = "text";
+    } else {
+      password.type = "password";
+    }
+  });
+
+  eyeIconConfirm.addEventListener("click", () => {
+    if (confirmPassword.type === "password") {
+      confirmPassword.type = "text";
+    } else {
+      confirmPassword.type = "password";
+    }
+  });
+};
+
+const app = () => {
+  handleButtonRegister();
+  handleShowPassword();
+};
+
+app();
